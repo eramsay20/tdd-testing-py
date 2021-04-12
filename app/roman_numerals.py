0ba39b49931d071cb301b7d94b3ca1492eddc07a
minkikim98 <minkikim98@gmail.com>
2021-04-12 15:44:56 -0700

    comprehensive parsing completed

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,6 +1,23 @@
+"XVI"
+"XC"
 
 def parse(roman_num):
-    if roman_num == "I":
-        return 1
-    elif roman_num == "II":
-        return 2
+    mappings = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    shortcuts = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
+    index = 0
+    total = 0
+    while index < len(roman_num):
+        curr_num = roman_num[index]
+        if index + 1 == len(roman_num):
+            total += mappings[curr_num]
+            index += 1
+        else:
+            next_num = roman_num[index + 1]
+            potential_shortcut = curr_num + next_num
+            if potential_shortcut in shortcuts:
+                total += shortcuts[potential_shortcut]
+                index += 2
+            else: 
+                total += mappings[curr_num]
+                index += 1
+    return total
